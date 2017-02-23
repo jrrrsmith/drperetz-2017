80032d6b27c88af3780c32a2e861810cfad115d3
jrrrsmith <jeremy.scott.smith@me.com>
2017-02-23 02:00:53 -0500

    elements section on home page

--- wp-content/themes/drperetz/assets/js/main.js
+++ wp-content/themes/drperetz/assets/js/main.js
@@ -1,61 +1,61 @@
 // Elements of Care tab functionality
 
-$(document).ready(function () {
-  $('.element-item__top').on('click', function(event) {
-    if ($(this).parent().hasClass('element-item--active')) {
-      event.preventDefault();
-    } else {
-      event.preventDefault();
-      $('.element-item--active').removeClass('element-item--active');
-      $(this).parent().addClass('element-item--active');
-    }
-  });
-});
+// $(document).ready(function () {
+//   $('.element-item__top').on('click', function(event) {
+//     if ($(this).parent().hasClass('element-item--active')) {
+//       event.preventDefault();
+//     } else {
+//       event.preventDefault();
+//       $('.element-item--active').removeClass('element-item--active');
+//       $(this).parent().addClass('element-item--active');
+//     }
+//   });
+// });
 
 
 // Elements of Care sliding panels
   //toggle panel slide
-$(document).ready(function () {
-  $('.elements-details__cover').on('click', function(event) {
-    $(this).siblings().toggleClass('element-slide');
-  });
-});
+// $(document).ready(function () {
+//   $('.elements-details__cover').on('click', function(event) {
+//     $(this).siblings().toggleClass('element-slide');
+//   });
+// });
 
   //close panel
 
-$(document).ready(function () {
-  $('.slide-up').on('click', function(event) {
-    $(this).parent().removeClass('element-slide');
-  });
-});
+// $(document).ready(function () {
+//   $('.slide-up').on('click', function(event) {
+//     $(this).parent().removeClass('element-slide');
+//   });
+// });
 
 // VIDEO RESIZE FUNCTION
 
-$(document).ready(function() {
-  var $allVideos = $("iframe[src^='https://player.vimeo.com'], iframe[src^='https://www.youtube.com']"),
+// $(document).ready(function() {
+//   var $allVideos = $("iframe[src^='https://player.vimeo.com'], iframe[src^='https://www.youtube.com']"),
 
-      $fluidEl = $(".home-video__wrap");
+//       $fluidEl = $(".home-video__wrap");
 
-  $allVideos.each(function() {
+//   $allVideos.each(function() {
 
-    $(this)
-      .data('aspectRatio', this.height / this.width)
+//     $(this)
+//       .data('aspectRatio', this.height / this.width)
 
-      .removeAttr('height')
-      .removeAttr('width');
+//       .removeAttr('height')
+//       .removeAttr('width');
 
-  });
+//   });
 
-  $(window).resize(function() {
+//   $(window).resize(function() {
 
-    var newWidth = $fluidEl.width();
-    $allVideos.each(function() {
+//     var newWidth = $fluidEl.width();
+//     $allVideos.each(function() {
 
-      var $el = $(this);
-      $el
-        .width(newWidth)
-        .height(newWidth * $el.data('aspectRatio'));
+//       var $el = $(this);
+//       $el
+//         .width(newWidth)
+//         .height(newWidth * $el.data('aspectRatio'));
 
-    });
-  }).resize();
-});
+//     });
+//   }).resize();
+// });
