61883862dcb230be16fbb055c656eef267d579d7
jrrrsmith <jeremy.scott.smith@me.com>
2017-04-19 03:08:45 -0400

    flowed text around photo

--- wp-content/themes/drperetz/page-templates/partials/about/details.php
+++
@@ -1,9 +0,0 @@
-<div class="about-details">
-  <div class="about-details__top">
-    <?php the_field('details'); ?>
-  </div>
-  <div class="about-details__bottom">
-    <h2>Education and Training</h2>
-    <?php the_field('education'); ?>
-  </div>
-</div>
