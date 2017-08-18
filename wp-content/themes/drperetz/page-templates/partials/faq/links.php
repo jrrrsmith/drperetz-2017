25668c0799f4f2f635b215070c7868cc4a7fb5d9
jrrrsmith <jeremy.scott.smith@me.com>
2017-08-18 18:01:52 -0400

    changes for p

--- wp-content/themes/drperetz/page-templates/partials/faq/links.php
+++ wp-content/themes/drperetz/page-templates/partials/faq/links.php
@@ -1,10 +1,16 @@
 <div class="faq-links">
   <h1>Psoriasis FAQs</h1>
-  <?php if( have_rows('links') ): ?>
-    <?php while( have_rows('links') ): the_row(); ?>
-    <div>
-      <a href="#<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
-    </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
+  <div class="faq-links__left">
+    <?php if( have_rows('links') ): ?>
+      <?php while( have_rows('links') ): the_row(); ?>
+      <div>
+        <a href="#<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
+      </div>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
+  <div class="faq-links__right">
+    <img src="<?php the_field('faq_img') ?>" />
+    <p class="faq-img-title"><?php the_field('faq_img_title') ?></p>
+  </div>
 </div>
