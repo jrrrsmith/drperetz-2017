97edc36522f3a75367578cad352e284b0bed2b20
jrrrsmith <jeremy.scott.smith@me.com>
2017-08-26 13:14:41 -0400

    changes to consult page

--- wp-content/themes/drperetz/page-templates/partials/consultation/cta.php
+++ wp-content/themes/drperetz/page-templates/partials/consultation/cta.php
@@ -1,14 +1,14 @@
 <div class="consult-body__cta">
   <div class="consult-body__cta__section">
     <div class="wrap">
-      <h1>I can't wait to begin!</h1>
+      <h1><?php the_field('cta_top_title'); ?></h1>
       <?php the_field('cta_top'); ?>
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>scheduler" class="button button--blue">Proceed to Scheduler</a>
     </div>
   </div>
   <div class="consult-body__cta__section">
     <div class="wrap">
-      <h1>Become a patient of Dr. Peretz</h1>
+      <h1><?php the_field('cta_bottom_title'); ?></h1>
       <?php the_field('cta_bottom'); ?>
       <a href="http://www.wholefamilymd.org" class="button button--green">Go to Whole Family MD</a>
     </div>
