5dc129e59bb1b8309d52d00b77f617133215a290
jrrrsmith <jeremy.scott.smith@me.com>
2017-08-26 13:18:09 -0400

    ACF for elements of care images and titls on home page

--- wp-content/themes/drperetz/page-templates/partials/home/elements.php
+++ wp-content/themes/drperetz/page-templates/partials/home/elements.php
@@ -1,14 +1,14 @@
 <div class="elements">
   <a href="<?php echo esc_url( home_url( '/' ) ); ?>elements-of-care" class="element-item">
-    <img src="<?php bloginfo('template_url');?>/assets/images/thumb-1.jpg" class="element-item__img" />
-    <h1>nourish</h1>
+    <img src="<?php the_field('element_one_image'); ?>" class="element-item__img" />
+    <h1><?php the_field('element_one_title'); ?></h1>
   </a>
   <a href="<?php echo esc_url( home_url( '/' ) ); ?>elements-of-care" class="element-item">
-    <img src="<?php bloginfo('template_url');?>/assets/images/thumb-2.jpg" class="element-item__img" />
-    <h1>energize</h1>
+    <img src="<?php the_field('element_two_image'); ?>" class="element-item__img" />
+    <h1><?php the_field('element_two_title'); ?></h1>
   </a>
   <a href="<?php echo esc_url( home_url( '/' ) ); ?>elements-of-care" class="element-item">
-    <img src="<?php bloginfo('template_url');?>/assets/images/thumb-3.jpg" class="element-item__img" />
-    <h1>restore</h1>
+    <img src="<?php the_field('element_three_image'); ?>" class="element-item__img" />
+    <h1><?php the_field('element_three_title'); ?></h1>
   </a>
 </div>
